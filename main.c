ee12b0e313d4464fe7e5a8a205d341049cbfb152
Dobrianovn <nikithepaperpilot@gmail.com>
2017-10-15 18:54:02 -0400

    A completed search tree with several functions.

--- main.c
+++ main.c
@@ -1,31 +1,72 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "tree.h"
 
-// ----------------------------------------------------------------------------
-//
-Node *createNode(const char *pStr)
+/*
+createNode is a method that returns a node pointer and returns a pointer with count 1 and
+input string as it's value and two null nodes left and right of it.
+*/
+Node *createNode(const char *pStrIn)
 {
-  // TODO
-  return 0;
+  Node *pTemp = (Node*)malloc(sizeof(Node));
+  pTemp->count = 1;
+  pTemp->pStr = (char*)malloc(sizeof(char)*(strlen(pStrIn) + 1));
+  strcpy(pTemp->pStr, pStrIn);
+  pTemp->pLeft = NULL;
+  pTemp->pRight = NULL;
+  return pTemp;
 }
 
-bool addString(Node *pRoot, const char *pStr, Node **ppNode /* = 0 */)
+/*
+addString adds an input string to the search tree. The strings are in a BST with a lexographical
+value comparison, with words lexographically first to the left and no duplicates but increasing
+the count on repeat entry (returns false on fail).
+*/
+bool addString(Node *pRoot, const char *pStrIn)
 {
-  // TODO
-  return false;
+  Node *pTemp = pRoot;
+  Node *previous;
+  while(pTemp != NULL){
+    if(strncmp(pStrIn, pTemp->pStr, 4096) < 0){
+      previous = pTemp;
+      pTemp = pTemp->pLeft;
+    } else if(strncmp(pStrIn, pTemp->pStr, 4096) > 0){
+      previous = pTemp;
+      pTemp = pTemp->pRight;
+    } else {
+      pTemp->count += 1;
+      return false;
+    }
+  }
+  if(strncmp(pStrIn, previous->pStr, 4096) < 0){
+    pTemp = createNode(pStrIn);
+    previous->pLeft = pTemp;
+  } else {
+    pTemp = createNode(pStrIn);
+    previous->pRight = pTemp;
+  }
+  return true;
 }
 
-void traverseDF(const Node *pRoot, void(callBack)(const Node *))
-{
-  // TODO
+//printNode is a method to print the string at a given node pointer
+void printNode(Node *pNode){
+  printf("%s : %d\n", pNode->pStr, pNode->count);
 }
-// ----------------------------------------------------------------------------
 
-// TODO: Define function printNode(...) with proper signature, so as to be
-// callable by traverseDF()!
-
-/* -------------------------------------
+/*
+traverseInOrder prints to the console in depth first in order format the string values
+at each node.
+*/
+void traverseInOrder(Node *pNode, void (callBack)(Node*)){
+  if(pNode == NULL){
+    return;
+  } 
+  traverseInOrder(pNode->pLeft, callBack);
+  callBack(pNode);
+  traverseInOrder(pNode->pRight, callBack);
+}
+/*-------------------------------------- 
 TESTING
 =======
 1. From the command line:
@@ -44,19 +85,21 @@ EOF
 int main(int argc, const char *argv[])
 {
   char buf[4096];
+  Node *searchTree = NULL;
   while (fgets(buf, sizeof buf, stdin))
   {
     // If the line was NOT truncated - remove the trailing newline
     if (buf[strlen(buf) - 1] == '\n')
       buf[strlen(buf) - 1] = 0;
-
-    printf(">>> %s\n", buf); // For testing/debugging
-
-    // TODO: Insert strings into tree
+    // printf(">>> %s\n", buf); // For testing/debugging
+    if(!searchTree){
+      searchTree = createNode(buf);
+      continue;
+    }
+    addString(searchTree, buf);
   }
-
+  traverseInOrder(searchTree, printNode);
   // TODO: Print the sorted lines, with counts:
   // traverseDF(pRoot, printNode);
-
   return 0;
 }
--- tree.h
+++ tree.h
@@ -9,7 +9,7 @@ struct Node
 };
 
 // Creates a Node with count = 1, pLeft and pRight = 0
-Node *createNode(const char *pStr);
+Node *createNode(const char *pStrIn);
 
 // Adds a string, unless the string is already in the tree.
 // Keeps the tree SORTED by string value.
@@ -18,7 +18,9 @@ Node *createNode(const char *pStr);
 //  - Return value is true if insertion took place, or false
 //  - ppNode may be 0 (aka. "NULL"). If not null, it should return the
 //    Node was found or freshly created.
-bool addString(Node *pRoot, const char *pStr, Node **ppNode /* = 0 */);
+bool addString(Node *pRoot, const char *pStrIn);
+
+void printNode(Node *pNode);
 
 // Traverse the tree depth-first and call callBack() for each node
-void traverseDF(const Node *pNode, void (callBack)(const Node *));
+void traverseInOrder(Node *pNode, void (callBack)(Node*));
