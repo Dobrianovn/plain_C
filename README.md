aa643ec0dc1caa25faad90f7c53c4d02547a6334
Ivan Dobrianov <gitpub16-f70@dobrianov.net>
2017-10-14 21:25:44 -0400

    Simple test case for arrays and ptrs

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# plain_C
-Plain old C snippets, tests, junk
+# This branch
+Simply illustrates how pointers and arrays are treated in `C`: memory layout, etc.
+
+The short of it is that pointers and array indentifiers can be treated in a very similar way, so long as one follows "the standard rules" and types and their sizes are respected. Both array and pointer arithmetic work as expected.
+
+# Program output
+On a 64-bit Linux gcc build, where the pointer size is 8, this is the output:
+```
+strings1 = aa
+sizeof strings1 = 3
+Memory at strings1:
+61 61 0
+
+pStrings1 = aaAA
+sizeof pStrings1 = 8
+Memory at pStrings1:
+61 61 41 41 0 6f 0 6f
+
+strings2 =
+[0] aaaaaaaa
+[1] b
+[2] cccXc
+Memory at strings2:
+61 61 61 61 61 61 61 61 0 0 62 0 0 0 0 0 0 0 0 0 63 63 63 58 63 0 0 0 0 0
+sizeof strings2 = 30
+sizeof *strings2 = 10
+sizeof strings2[0] = 10
+Pointer arithmetic: *(*(strings2 + 2) + 3) = 'X' = 58
+
+pStrings2 =
+[0] o
+[1] oo
+[2] oooYo
+Memory at pStrings2:
+4d a 40 0 0 0 0 0 4f a 40 0 0 0 0 0 52 a 40 0 0 0 0 0
+sizeof pStrings2 = 24
+sizeof *pStrings2 = 8
+sizeof pStrings2[0] = 8
+Pointer arithmetic: *(*(pStrings2 + 2) + 3) = 'Y' = 59
+
+```
+Enjoy interpreting the numbers!
--- main.c
+++ main.c
@@ -1,62 +1,61 @@
+// gcc -g -std=c99 main.c
 #include <stdio.h>
 #include <string.h>
-#include "tree.h"
 
-// ----------------------------------------------------------------------------
-//
-Node *createNode(const char *pStr)
-{
-  // TODO
-  return 0;
-}
+#define prnsz(xx) printf("sizeof " #xx " = %ld\n", sizeof(xx))
 
-bool addString(Node *pRoot, const char *pStr, Node **ppNode /* = 0 */)
-{
-  // TODO
-  return false;
-}
+#define memAt(xx)                               \
+  printf("Memory at " #xx ":\n");               \
+  for (ii = 0; ii < sizeof xx; ++ii)            \
+  {                                             \
+    printf("%x ", *((unsigned char *)xx + ii)); \
+  }                                             \
+  printf("\n")
 
-void traverseDF(const Node *pRoot, void(callBack)(const Node *))
-{
-  // TODO
-}
-// ----------------------------------------------------------------------------
+#define prnArr(xx)                                \
+  printf(#xx " =\n");             \
+  for (ii = 0; ii < sizeof xx / sizeof *xx; ++ii) \
+  {                                               \
+    printf("[%d] %s\n", ii, xx[ii]);               \
+  }
 
-// TODO: Define function printNode(...) with proper signature, so as to be
-// callable by traverseDF()!
-
-/* -------------------------------------
-TESTING
-=======
-1. From the command line:
-cat text2.txt | ./a.out
-   _OR_
-./a.out <<EOF
-blahh
-dah
-blahh
-EOF
-
-2. From the debugger or command line, if not feeding a file, just type
-   words and then ctrl-D for EOF to signal the end.
--------------------------------------  */
+#define ptrArith(xx, ii, jj) \
+  printf("Pointer arithmetic: *(*(" #xx " + " #ii ") + " #jj ") = '%c' = %x\n", \
+  *(*(xx + ii) + jj), *(*(xx + ii) + jj));
 
 int main(int argc, const char *argv[])
 {
-  char buf[4096];
-  while (fgets(buf, sizeof buf, stdin))
-  {
-    // If the line was NOT truncated - remove the trailing newline
-    if (buf[strlen(buf) - 1] == '\n')
-      buf[strlen(buf) - 1] = 0;
+  int ii;
 
-    printf(">>> %s\n", buf); // For testing/debugging
+  const char strings1[] = "aa";
+  const char *pStrings1 = "aaAA";
+  const char strings2[3][10] = {"aaaaaaaa", "b", "cccXc"};
+  const char *pStrings2[3] = {"o", "oo", "oooYo"};
 
-    // TODO: Insert strings into tree
-  }
+  printf("strings1 = %s\n", strings1);
+  prnsz(strings1);
+  memAt(strings1);
 
-  // TODO: Print the sorted lines, with counts:
-  // traverseDF(pRoot, printNode);
+  printf("\n");
+  printf("pStrings1 = %s\n", pStrings1);
+  prnsz(pStrings1);
+  memAt(pStrings1);
+
+  printf("\n");
+  prnArr(strings2);
+  memAt(strings2);
+  prnsz(strings2);
+  prnsz(*strings2);
+  prnsz(strings2[0]);
+  ptrArith(strings2, 2, 3);
+
+  printf("\n");
+  prnArr(pStrings2);
+  memAt(pStrings2);
+  prnsz(pStrings2);
+  prnsz(*pStrings2);
+  prnsz(pStrings2[0]);
+  ptrArith(pStrings2, 2, 3);
 
   return 0;
 }
